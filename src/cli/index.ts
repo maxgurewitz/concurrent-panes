2b46b587fc58bcc3445999e7ee6e93aa3d65a018
maxgurewitz <maxthegeek1@gmail.com>
2017-07-29 00:48:22 -0700

    messages pushed to client

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -5,7 +5,7 @@ import * as through from 'through';
 const process = global.process;
 
 function runTask(taskname: string) {
-  return childProcess.spawn('sh', [taskname], {
+  return childProcess.spawn('sh', ['-c', taskname], {
     env: process.env,
     cwd: process.cwd()
   });
@@ -17,8 +17,9 @@ interface Callback {
 
 class CodedError extends Error {
   code: number;
+  task: string;
 
-  constructor(code: number, message=`Exited with code ${code}`) {
+  constructor(task: string, code: number, message=`${task} exited with code ${code}`) {
     super(message);
     this.code = code;
   }
@@ -43,27 +44,32 @@ export default function parallel(tasks: string[], done: Callback) {
   const wss = new WebSocket.Server({ port: 3001 });
 
   wss.on('connection', ws => {
-    ws.on('message', message => {
-    });
+    ws.send(JSON.stringify({ type: 'init', tasks: tasks }));
 
     tasks.forEach(task => {
       const proc = runTask(task);
 
-      proc.on('error', err => doneErr(new CodedError(1, err.message)));
+      proc.on('error', err => doneErr(new CodedError(task, 1, err.message)));
 
       proc.on('close', code => {
         if (code !== 0) {
-          const err = new CodedError(code);
+          const err = new CodedError(task, code);
           doneErr(err);
         } else {
           doneOk();
         }
       })
 
+      const onSendErr = (err: Error) => {
+        if (err) {
+          console.error('failed to send message', err);
+        }
+      };
+
       function sendMessage(task: string, outputType: string): through.ThroughStream {
         return through(
-          buffer => ws.send(JSON.stringify({ task, outputType, message: String(buffer) })),
-            () => ws.send(JSON.stringify({}))
+          buffer => ws.send(JSON.stringify({ type: 'output', task, outputType, message: String(buffer) }), onSendErr),
+            () => ws.send(JSON.stringify({ type: 'end', outputType }), onSendErr)
         );
       }
 
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,27 +1,68 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-interface Source {}
+interface Source {
+  content: string
+}
 
-interface State { sources: Source[] }
+interface State {
+  [task: string]: Source
+}
 
 class App extends React.Component<undefined, State> {
 
-  websocket: WebSocket;
-
   constructor() {
     super();
-    
-    console.log('loc1');
-    this.websocket = new WebSocket('ws://localhost:3001');
 
-    this.state = {
-      sources: []
-    };
+    const websocket: WebSocket = new WebSocket('ws://localhost:3001');
+
+    websocket.onmessage = event => {
+      const data = JSON.parse(event.data);
+
+      switch (data.type) {
+        case 'init':
+          this.setState((previousState: State) => {
+            data.tasks.forEach((task: string) => {
+              previousState[task] = { content: '' };
+            });
+            return previousState;
+          });
+          break;
+
+        case 'output':
+          this.setState((previousState: State) => {
+            const previousTask = previousState[data.task];
+            previousTask.content = previousTask.content + data.message;
+            return previousState;
+          });
+          break;
+      }
+    }
+
+    this.state = {};
   }
 
   render() {
-    return <h1>hello world</h1>;
+    const panes = Object.keys(this.state).map((task, i) => {
+      const source = this.state[task];
+      return (
+        <div key={i}>
+          <div> { task } </div>
+          <div>
+            { source.content }
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <div>
+        <div> concurrent-panes </div>
+        <div>
+          { panes }
+        </div>
+      </div>
+    );
   }
 }
 
